8c5f8090989ce2ae72c86c6e4d9d1891d6bbbf1e
Yash Goyal <goyal.yash18335@gmail.com>
2026-08-18 10:17:38 +0530

    Time: 1 ms (83.33%) | Memory: 3.8 MB (65.19%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.go
+++ 1013-fibonacci-number/fibonacci-number.go
@@ -1,14 +1,12 @@
 func fib(n int) int {
-    if n <= 1 {
+    if n == 0 || n == 1 {
         return n
     }
 
-    var n1 int = 0
-    var n2 int = 1
+    n1, n2 := 0, 1
 
     for i := 2; i <= n; i++ {
-
-        var curr int = n2 + n1
+        curr := n1 + n2
 
         n1 = n2
         n2 = curr
