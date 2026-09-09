476e21c930a4f1eb134d82dc102c960cfa6e7496
Yash Goyal <goyal.yash18335@gmail.com>
2026-09-09 11:06:50 +0530

    Time: 358 ms (56.84%) | Memory: 75.6 MB (41.77%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -11,4 +11,4 @@ class Solution:
                     dp[i][j] = dp[i+1][j+1] + dp[i+1][j]
                 else:
                     dp[i][j] = dp[i+1][j]
-        return dp[i][j]
+        return dp[0][0]
