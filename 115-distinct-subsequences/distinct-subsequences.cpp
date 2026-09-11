1552147792e1894bd90a84daa77b8382769aebaa
Yash Goyal <goyal.yash18335@gmail.com>
2026-09-11 22:33:18 +0530

    Time: 34 ms (30.20%) | Memory: 44.3 MB (18.60%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -6,8 +6,8 @@ public:
         for(int i = 0; i <= s.length(); i++) {
             dp[i][t.length()] = 1;
         }
-        for(int i = s.length()-1; i >= 0; i--) {
-            for(int j = t.length()-1; j >= 0; j--) {
+        for(int i = s.length()-1; i >= 0; --i) {
+            for(int j = t.length()-1; j >= 0; --j) {
                 if(s[i] == t[j]) {
                     if(dp[i+1][j+1] > INT_MAX - dp[i+1][j]) {
                         dp[i][j] = INT_MAX;
