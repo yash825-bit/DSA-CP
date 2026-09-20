6ac0589da9811c1e321e67ef85d07f6023b6b6cd
Yash Goyal <goyal.yash18335@gmail.com>
2026-09-21 00:00:42 +0530

    Time: 4 ms (9.52%) | Memory: 9.9 MB (15.78%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int reverseDegree(string s) {
         int sum = 0;
-
-        int codes[] = {26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-
+        
         for(int i = 0; i < s.size(); i++) {
             sum += ((-1*(s[i]-'z'))+1)*(i+1);
         }
